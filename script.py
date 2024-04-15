be944c990037ac87896474afb74440e708d5d277
migueliiin <91721552+MiguelGG03@users.noreply.github.com>
2024-04-15 17:05:21 +0200

    dataframe para Paraview

--- script.py
+++ script.py
@@ -1,3 +1,9 @@
 import numpy as np
+import pandas as pd
+
 domain = np.zeros((10, 10, 10))  # Crea un dominio 3D de 10x10x10
 print(domain)
+
+
+df = pd.DataFrame({'x': [0, 1], 'y': [0, 1], 'z': [0, 1], 'pressure': [100, 150]})
+df.to_csv('output.csv', index=False)
